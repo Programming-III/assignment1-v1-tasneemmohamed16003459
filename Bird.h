1da3bd1afa9d5254b6f2d859a09e10e94cabcc99
tasneemmohamed16003459 <tasneem.ayman@student.giu-uni.de>
2025-11-12 14:58:43 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,19 @@
-#ifndef BIRD_H
-#define BIRD_H
-
+#ifndef BIRD
+#define BIRD
 #include "Animal.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
-//define class bird here 
-
-
-
-
-
-
+class Bird : public Animal {
+    private:
+    float wingspan;
+    
+    public:
+    Bird();
+    Bird(string n, int a, bool isH, float w);
+    ~Bird();
+   
+};
 
 #endif
